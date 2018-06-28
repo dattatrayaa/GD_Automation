3f266d5a1d01c3664fd899d42af2ac49832b4930
QAutomat <36909286+QAutomat@users.noreply.github.com>
2018-06-28 17:22:29 +0530

    Add files via upload

---
+++ TestCases/Admin/Roles/RoleReportAdministratorBrandIntegrate.py
@@ -0,0 +1,129 @@
+'''
+Created on 05-Apr-2018
+
+@author: Sheethu C
+'''
+
+
+
+from operator import contains
+import os.path
+import time
+import traceback
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+import xlrd
+
+from BaseTestClass import BaseTestClass
+from BaseTestClass import driver
+from DeleteRole import DeleteRole
+from BaseTestClass import projectPath
+from RoleXpathElements import RoleXpathElements
+from BaseTestClass import excelPath
+class RoleReportAdministratorBrandIntegrate():
+    
+    def createReportAdministratorBrandIntegrate(self,RoleName,Description):
+        createrole =RoleXpathElements()
+        wait=WebDriverWait(driver, 80)
+        driver.refresh()
+        wait.until(EC.visibility_of_element_located((By.XPATH,createrole.adminSideMenu())))
+        driver.find_element_by_xpath(createrole.adminSideMenu()).click()
+        print "Clicked on admin page"
+        wait.until(EC.visibility_of_element_located((By.XPATH,createrole.roleSideMenu())))
+        driver.find_element_by_xpath(createrole.roleSideMenu()).click()
+        print "Clicked on Role icon"
+        wait.until(EC.visibility_of_element_located((By.XPATH,createrole.createRole())))
+        driver.find_element_by_xpath(createrole.createRole()).click()
+        print "Clicked on Create role Button"
+        createrole.roleCreation(RoleName,Description)
+        #report
+        createrole.reportClick()
+        #admin
+        createrole.adminClick()
+        
+        
+        
+        print "Clicked on Administrator "
+        ch=RoleXpathElements()
+        ch.checkReport()
+        time.sleep(4)
+        ch.checkAdministrator()
+        time.sleep(4)
+        #User
+        createrole.userClick()
+        
+        #Content
+        createrole.contentClick()
+        
+        #Role
+        createrole.roleClick()
+       
+        #Tag
+        createrole.tagClick()
+        
+        time.sleep(3)
+        print "Checking Check box selection"
+        chk=RoleXpathElements()
+        chk.checkboxBranding()
+        print "Branding is selected"
+        chk.checkboxIntegration()
+        print "Integration is selected"
+        createrole.roleSave()
+        print "Clicked on Save Role Button"
+        print "Searching for the Created Role in the List"
+        createrole.roleSearch(RoleName)
+    def createReportAdministratorBrandIntegrateMain(self):  
+        try:   
+            book=xlrd.open_workbook(os.path.join('Test_Data/TestData.xlsx'))
+            sheet=book.sheet_by_name('Role')
+            cell = sheet.cell(380,1)
+            RoleName = cell.value
+            cell = sheet.cell(381,1)
+            Description = cell.value
+            book=xlrd.open_workbook(os.path.join('Test_Data/TestData.xlsx'))
+            first_sheet = book.sheet_by_name('UserAssignToRole')
+            cell = first_sheet.cell(2,1)
+            FirstName = cell.value
+            obj2= RoleReportAdministratorBrandIntegrate()
+            obj2.createReportAdministratorBrandIntegrate(RoleName,Description)  
+            User=RoleXpathElements()
+            User.CreateUserReport()
+            User.searchRole(RoleName,FirstName)
+            User.deactivateUser(FirstName)
+            rolede=DeleteRole()
+            rolede.roleDelete(RoleName)
+      
+     
+            #if any alert box occurs it will accept the alert
+            #if any alert box occurs it will accept the alert
+        except Exception as e:
+            traceback.print_exc()
+            print (e)
+            raise Exception   
+        finally:
+            User=RoleXpathElements() 
+            User.updateUserReport()   
+            second_sheet = book.sheet_by_name('Login_Credentials')
+            cell = second_sheet.cell(1,1)
+            url = cell.value
+            driver.get(url)
+            #if any alert box occurs it will accept the alert
+            try:
+                WebDriverWait(driver, 5).until(EC.alert_is_present())
+
+                alert = driver.switch_to.alert
+                alert.accept()
+                print("alert accepted")
+            except Exception:
+                print("no alert")
+        
+if __name__ == '__main__':
+    
+    obj11= ReportAdministratorBrandIntegrate()
+    obj12= BaseTestClass()
+    obj12.UserLogin()
+    obj11.createReportAdministratorBrandIntegrateMain()
---
+++ TestCases/Admin/Roles/RoleReportAdministratorContentTagIntegrate.py
@@ -0,0 +1,126 @@
+'''
+Created on 05-Apr-2018
+
+@author: Sheethu C
+'''
+from operator import contains
+import os.path
+import time
+import traceback
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+import xlrd
+
+from BaseTestClass import BaseTestClass
+from BaseTestClass import driver
+from DeleteRole import DeleteRole
+from BaseTestClass import projectPath
+from RoleXpathElements import RoleXpathElements
+from BaseTestClass import excelPath
+class RoleReportAdministratorContentTagIntegrate():
+    
+    def createReportAdministratorContentTagIntegrate(self,RoleName,Description):
+        createrole =RoleXpathElements()
+        wait=WebDriverWait(driver, 80)
+        driver.refresh()
+        wait.until(EC.visibility_of_element_located((By.XPATH,createrole.adminSideMenu())))
+        driver.find_element_by_xpath(createrole.adminSideMenu()).click()
+        print "Clicked on admin page"
+        wait.until(EC.visibility_of_element_located((By.XPATH,createrole.roleSideMenu())))
+        driver.find_element_by_xpath(createrole.roleSideMenu()).click()
+        print "Clicked on Role icon"
+        wait.until(EC.visibility_of_element_located((By.XPATH,createrole.createRole())))
+        driver.find_element_by_xpath(createrole.createRole()).click()
+        print "Clicked on Create role Button"
+        createrole.roleCreation(RoleName,Description)
+        #report
+        createrole.reportClick()
+        #admin
+        createrole.adminClick()  
+       
+        
+       
+        print "Clicked on Administrator "
+        time.sleep(3)
+        ch=RoleXpathElements()
+        ch.checkReport()
+        time.sleep(4)
+        ch.checkAdministrator()
+        time.sleep(4)
+        #user
+        createrole.userClick()
+        
+        #role
+        createrole.roleClick()
+        
+        #Brand
+        createrole.brandingClick()
+        
+        print "Checking Check box selection"
+        chk=RoleXpathElements()
+        chk.checkboxContent()
+        print "Content Manger is selected"
+        chk.checkboxTag()
+        print "Tags is selected"
+        chk.checkboxIntegration()
+        print "Integration is selected"
+        createrole.roleSave()
+        time.sleep(3)
+        print "Clicked on Save Role Button"
+        print "Searching for the Created Role in the List"
+        createrole.roleSearch(RoleName)
+    def maincreateReportAdministratorContentTagIntegrate(self):  
+        try:   
+            book=xlrd.open_workbook(os.path.join('Test_Data/TestData.xlsx'))
+            sheet=book.sheet_by_name('Role')
+            cell = sheet.cell(396,5)
+            RoleName = cell.value
+            cell = sheet.cell(397,5)
+            Description = cell.value
+            book=xlrd.open_workbook(os.path.join('Test_Data/TestData.xlsx'))
+            first_sheet = book.sheet_by_name('UserAssignToRole')
+            cell = first_sheet.cell(2,1)
+            FirstName = cell.value
+            obj2= RoleReportAdministratorContentTagIntegrate()
+            obj2.createReportAdministratorContentTagIntegrate(RoleName,Description)  
+            User=RoleXpathElements()
+            User.CreateUserReport()
+            User.searchRole(RoleName,FirstName)
+            User.deactivateUser(FirstName)
+            rolede=DeleteRole()
+            rolede.roleDelete(RoleName)
+      
+     
+            #if any alert box occurs it will accept the alert
+            #if any alert box occurs it will accept the alert
+        except Exception as e:
+            traceback.print_exc()
+            print (e)
+            raise Exception  
+        finally:
+            User=RoleXpathElements() 
+            User.updateUserReport()   
+            second_sheet = book.sheet_by_name('Login_Credentials')
+            cell = second_sheet.cell(1,1)
+            url = cell.value
+            driver.get(url)
+            #if any alert box occurs it will accept the alert
+            try:
+                WebDriverWait(driver, 5).until(EC.alert_is_present())
+
+                alert = driver.switch_to.alert
+                alert.accept()
+                print("alert accepted")
+            except Exception:
+                print("no alert")
+        
+if __name__ == '__main__':
+    
+    obj11= ReportAdministratorContentTagIntegrate()
+    obj12= BaseTestClass()
+    obj12.UserLogin()
+    obj11.maincreateReportAdministratorContentTagIntegrate()
