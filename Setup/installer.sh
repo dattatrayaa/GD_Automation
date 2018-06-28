692399c416eafb839fcfb35dcf69b347962b8a32
QAutomat <36909286+QAutomat@users.noreply.github.com>
2018-06-28 09:55:44 +0530

    Update installer.sh

--- Setup/installer.sh
+++ Setup/installer.sh
@@ -1,3 +1,5 @@
+#Installation steps
+#Install Python 2.7.14  You can download latest version from https://www.python.org/getit
 sudo pip install robotframework
 pip install robotframework-requests
 pip install xlrd==1.1.0
