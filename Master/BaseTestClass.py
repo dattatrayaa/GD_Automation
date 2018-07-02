15cb9846580f775f2e2fc0d9896c95434ac8208e
QAutomat <36909286+QAutomat@users.noreply.github.com>
2018-07-02 12:54:40 +0530

    Update BaseTestClass.py

--- Master/BaseTestClass.py
+++ Master/BaseTestClass.py
@@ -22,8 +22,8 @@ mPath=sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'Master'))
 emailPath=sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'Test_Results'))
 
 lesnPath=sys.path.append(os.path.join(os.path.dirname(__file__), '..', '/TestCases/Create/Lesson'))
-chromepath=os.path.join(os.path.dirname(__file__), '..', 'Setup')
-driver=webdriver.Chrome('chromedriver')
+#chromepath=os.path.join(os.path.dirname(__file__), '..', 'Setup')
+driver=webdriver.Chrome("chromedriver")
 
 class BaseTestClass:
 
