241e087a66ed154f58c8dd75aaa3a665246c8d2b
QAutomat <36909286+QAutomat@users.noreply.github.com>
2018-07-09 02:10:26 -0400

    Update startSuite.sh

--- Test_Suite/startSuite.sh
+++ Test_Suite/startSuite.sh
@@ -1,9 +1,9 @@
 echo "Starting Robot suite"
 
-#pybot -n non-critical -d Test_Results -o lesson.xml -l loglesson.html -r lessonreport.html  ./Test_Suite/TestSuiteLessons.robot 
+pybot -n non-critical -d Test_Results -o lesson.xml -l loglesson.html -r lessonreport.html  ./Test_Suite/TestSuiteLessons.robot 
 #pybot -n non-critical -d Test_Results -o cmp.xml -l logcmp.html -r cmpreport.html ./Test_Suite/TestSuiteCampaigns.robot
 
-pybot -n non-critical -d Test_Results -o lesson.xml -l loglesson.html -r reportlesson.html  ./Test_Suite/TestSuiteLesson.robot 
+#pybot -n non-critical -d Test_Results -o lesson.xml -l loglesson.html -r reportlesson.html  ./Test_Suite/TestSuiteLesson.robot 
 
 #pybot -n non-critical -d Test_Results -o track.xml -l logtrack.html -r reporttrack.html  ./Test_Suite/TestSuiteTrack.robot
 
