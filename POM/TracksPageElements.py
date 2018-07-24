a5079ca072965b4d3dcbb15cc0d946c3b0d7f751
QAutomat <36909286+QAutomat@users.noreply.github.com>
2018-07-24 10:47:08 +0530

    Update TracksPageElements.py

--- POM/TracksPageElements.py
+++ POM/TracksPageElements.py
@@ -10,7 +10,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
-from __main__ import traceback
+import traceback
 
 
 class TracksPageElements:
